778b72dff18befa0700495bbfe7b12c0554574d2
AyushmanKeshari <ayushiit23@gmail.com>
2022-07-10 00:03:53 +0530

    Adapter Pattern challenges and solution with Weather Warnings example

---
+++ src/StructuralDesignPatterns/AdapterPattern/Challenges_WeatherWarnings/Main.java
@@ -0,0 +1,20 @@
+package StructuralDesignPatterns.AdapterPattern.Challenges_WeatherWarnings;
+
+public class Main {
+    public static void main(String[] args) {
+        WeatherWarnings weatherWarnings = new WeatherWarnings();
+
+        NorthAmericanCity chicago = new NorthAmericanCity("Chicago", 16);
+        weatherWarnings.postWarning(chicago);
+
+        NorthAmericanCity phoenix = new NorthAmericanCity("Phoenix", 104);
+        weatherWarnings.postWarning(phoenix);
+
+        NorthAmericanCity portland = new NorthAmericanCity("Portland", 70);
+        weatherWarnings.postWarning(portland);
+
+        AsianCity bangkok = new AsianCity("Bangkok", 50);
+        //Should give warning. But not because checking in Fahrenheit.
+        weatherWarnings.postWarning(bangkok);
+    }
+}
---
+++ src/StructuralDesignPatterns/AdapterPattern/Solution_WeatherWarnings/Main.java
@@ -0,0 +1,23 @@
+package StructuralDesignPatterns.AdapterPattern.Solution_WeatherWarnings;
+
+public class Main {
+    public static void main(String[] args) {
+        WeatherWarnings weatherWarnings = new WeatherWarnings();
+
+        NorthAmericanCity chicago = new NorthAmericanCity("Chicago", 16);
+        weatherWarnings.postWarning(chicago);
+
+        NorthAmericanCity phoenix = new NorthAmericanCity("Phoenix", 104);
+        weatherWarnings.postWarning(phoenix);
+
+        NorthAmericanCity portland = new NorthAmericanCity("Portland", 70);
+        weatherWarnings.postWarning(portland);
+
+        AsianCity bangkok = new AsianCity("Bangkok", 50);
+        //Should give warning. But not because checking in Fahrenheit.
+        weatherWarnings.postWarning(bangkok);
+
+        Adapter adapter = new Adapter(bangkok);
+        weatherWarnings.postWarning(adapter);
+    }
+}
